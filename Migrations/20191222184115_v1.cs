325e830ead81985ccc1d0a4517c41cb32edea075
Bruno Oliveira <brunooliveira812@gmail.com>
2019-12-22 16:17:20 -0300

    first commit

---
+++ Migrations/20191222184115_v1.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductCatalog.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prodocuts_Categories_CategoryId",
+                table: "Prodocuts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Prodocuts",
+                table: "Prodocuts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Prodocuts",
+                newName: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Prodocuts_CategoryId",
+                table: "Product",
+                newName: "IX_Product_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Product",
+                type: "varchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Product",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Product",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Category",
+                type: "varchar",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Prodocuts");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CategoryId",
+                table: "Prodocuts",
+                newName: "IX_Prodocuts_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Prodocuts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Prodocuts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Prodocuts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Prodocuts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldMaxLength: 120);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Prodocuts",
+                table: "Prodocuts",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prodocuts_Categories_CategoryId",
+                table: "Prodocuts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20191222184343_correcao_tamanho_title.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductCatalog.Migrations
+{
+    public partial class correcao_tamanho_title : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Category",
+                type: "varchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldMaxLength: 120);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Category",
+                type: "varchar",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(120)",
+                oldMaxLength: 120);
+        }
+    }
+}
